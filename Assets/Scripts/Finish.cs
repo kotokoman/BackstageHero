7d6fed0aefcaa90892fbc81b35350b732c42d26c
kotokoman <yurikotoman@gmail.com>
2019-12-20 15:24:18 -0300

    Final

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "PlayLine")
+        {
+            FindObjectOfType<SceneManagement>().FinishGame();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+    private Scene currentScene;
+    private string sceneName;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && sceneName == "TitleScreen")
+        {
+            StartGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && (sceneName == "Level 01" || sceneName == "Finish"))
+        {
+            TitleScreen();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && sceneName == "Finish")
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 01");   //carrega cena de gameplay após start
+    }
+
+    public void FinishGame()
+    {
+        SceneManager.LoadScene("Finish");
+    }
+
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+
+}
